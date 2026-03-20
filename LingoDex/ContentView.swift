57e84a54d830b10632843f5b385e24d3efa2d6a6
Jia Yun Zhao <zhaojiayun565@gmail.com>
2026-03-19 21:16:40 -0400

    Initial Commit

---
+++ LingoDex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LingoDex
+//
+//  Created by Jia Yun Zhao on 2026-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LingoDex/LingoDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  LingoDexApp.swift
+//  LingoDex
+//
+//  Created by Jia Yun Zhao on 2026-03-19.
+//
+
+import SwiftUI
+
+@main
+struct LingoDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
